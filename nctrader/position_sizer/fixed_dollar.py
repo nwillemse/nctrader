c82ef670a8c8180b3a6dc87aeea3697b59616276
Nick Willemse <nwillemse@nct-cm.com>
2017-01-28 09:55:51 -0600

    Remove unused imports Add fixed_amount position sizer to allow for standard positions to be taken, i.e. invest $10,000 per position Start indicator class for common technical indicators

---
+++ nctrader/position_sizer/fixed_dollar.py
@@ -0,0 +1,33 @@
+from .base import AbstractPositionSizer
+
+
+class FixedDollarPositionSizer(AbstractPositionSizer):
+    def __init__(
+            self, dollar_amount
+    ):
+        self.dollar_amount = dollar_amount
+
+
+    def size_order(self, portfolio, initial_order):
+        """
+        This FixedDollarPositionSizer object modifies the quantity base on
+        how many shares or contracts can be purchased for dollar_amount.
+        """
+        if initial_order is None:
+            return
+        
+        ticker_info = portfolio.price_handler.tickers_info[initial_order.ticker]
+        last_price = portfolio.price_handler.get_last_close(initial_order.ticker)
+
+        # only size the order if quantity is zero - entry order
+        if initial_order.quantity == 0:
+            if ticker_info.type == 'STK':
+                quantity = int(self.dollar_amount / last_price)
+            elif ticker_info.type == 'FUT':
+                quantity = int(self.dollar_amount / ticker_info.margin)
+            else:
+                print "Ticker type not handled for", ticker_info
+        
+            initial_order.quantity = quantity
+
+        return initial_order
--- nctrader/position_sizer/fractional.py
+++ nctrader/position_sizer/fractional.py
@@ -1,4 +1,3 @@
-import numpy as np
 from .base import AbstractPositionSizer
 
 
